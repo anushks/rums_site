41296fb4151721c9aeab89731335b5c60b9c2d11
anushks <anushks@users.noreply.github.com>
2017-12-28 16:22:27 +0000

    fix modal scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $('[class!=modal-trigger]').on('click', function(event) {
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
